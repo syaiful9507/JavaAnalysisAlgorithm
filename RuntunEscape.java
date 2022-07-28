0331a3fc4aad989d7e8dc8aca914e308fc3e635d
SYAIFUL <syaiful9507@gmail.com>
2022-07-28 10:30:57 +0700

    Escape

---
+++ RuntunEscape.java
@@ -0,0 +1,33 @@
+public class RuntunEscape {
+    public static void main(String[] args) {
+
+        /*Runtun Escape
+         * --------------------------------------
+         * | Runtun Escape  | meaning           |
+         * --------------------------------------
+         * | \b             | backspace         |
+         * --------------------------------------
+         * | \t             | tab               |
+         * --------------------------------------
+         * | \n             | newline           |
+         * --------------------------------------
+         * | \r             | carriage return   |
+         * --------------------------------------
+         * | \"             | kutip ganda       |
+         * --------------------------------------
+         * | \'             | kutip tunggal     |
+         * --------------------------------------
+         * | \\             | backslash         |
+         * --------------------------------------
+         */
+
+         System.out.println("Mawar Merah, \n \tAnggrek biru, \n" +
+         "Gula manis, \n\tSayang aku bermasalah \"dengan komitmen\", \n\t" +
+         "Jadi lebih baik aku hanya sahabat\n\tDi titik ini dalam " +
+         "Hubungan Kita. ");
+
+        
+    }
+}
+
+
