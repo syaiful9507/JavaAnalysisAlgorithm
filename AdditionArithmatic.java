e761930d511f02110b8b54a5b7a9d2e33aaf0d04
SYAIFUL <syaiful9507@gmail.com>
2022-07-28 10:12:52 +0700

    Addition Arithmatic

---
+++ AdditionArithmatic.java
@@ -0,0 +1,10 @@
+/**
+ * AdditionArithmatic
+ */
+public class AdditionArithmatic {
+
+    public static void main(String[] args) {
+        System.out.println("24 dan 45 disambungkan :  " + 24 + 45 );
+        System.out.println("24 dan 45 dijumlahkan : " + (24+45));
+    }
+}
