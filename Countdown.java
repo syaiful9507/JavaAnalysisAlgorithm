9f486eab22963118e9b4210d03354184c19f1a97
SYAIFUL <syaiful9507@gmail.com>
2022-07-28 09:36:14 +0700

    Different Print and println

---
+++ Countdown.java
@@ -0,0 +1,17 @@
+//Different print and println
+/**
+ * Countdown
+ */
+public class Countdown {
+
+    public static void main(String[] args) {
+        System.out.print("Three. . .");
+        System.out.print("Two. . .");
+        System.out.print("One. . .");
+        System.out.print("Zero. . .");
+        System.out.println("Raise !");
+        System.out.println("Let Go, Don't Lazy ! .");
+
+
+    }
+}
