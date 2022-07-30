392f8777a6d67c7d826795e4753717d068aebe1d
SYAIFUL <syaiful9507@gmail.com>
2022-07-30 16:57:15 +0700

    Ex

---
+++ Exercise.java
@@ -0,0 +1,11 @@
+/**
+ * Exercise
+ */
+public class Exercise {
+
+    public static void main(String[] args) {
+        
+        System.out.println();
+        System.out.println("Coba Tebak ini : " + (5+5));
+    }
+}
