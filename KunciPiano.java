2b9b5071d8c880403115cc225aa343bbfd711080
SYAIFUL <syaiful9507@gmail.com>
2022-08-01 10:39:18 +0700

    Variable

---
+++ KunciPiano.java
@@ -0,0 +1,16 @@
+/**
+ * KunciPiano
+ * Mendemonstrasikan deklarasi, inisialisasi, dan kegunaan dari variable Integer
+ */
+public class KunciPiano {
+
+    //Manampilan jumlah kunci dari sebuah piano
+    public static void main(String[] args) {
+        int kunci = 88;
+        System.out.println("Piano memiliki sebanyak " + kunci + " Kunci");
+        /*
+         * Ketika pemanggilan println dieksekusi nilai dari kunci yang bernilai 88 diperoleh karena nilai itu sebuah Integer,
+         * Ia secara otomatis dikonversi ke string dan disambung dengan string awal
+         */
+    }
+}
